7f83728aaad54de881f6d6f2c91251d8f0d8b711
Oliverhenri <henriqueoliv.contato@gmail.com>
2022-07-08 15:15:49 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Retorne no console todas as imagens do site
+const imagensSite = document.querySelectorAll('img');
+console.log(imagensSite);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensImagens = document.querySelector('[href^="img"]');
+console.log(imagensImagens);
+
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
+
